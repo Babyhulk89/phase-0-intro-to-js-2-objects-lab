9ee6d8d1969bc8860e35e20ec9ba47085fdb5c2a
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-11-08 19:24:52 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Define the employee object
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St",
+  };
+  
+  // Define the functions
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  // Export the functions if you plan to use them in other files
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
+  
